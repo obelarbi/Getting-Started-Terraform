f6986817b052e0c526edd0bc86f70fb5995e8738
Omar Belarbi <omar.belarbi@gmail.com>
2021-02-04 21:54:16 +0100

    test variable

--- m3/module_three.tf
+++ m3/module_three.tf
@@ -16,7 +16,7 @@ variable "region" {
 ##################################################################################
 
 provider "aws" {
-  access_key = var.aws_access_key
+  access_key = var.aws_access_key_test
   secret_key = var.aws_secret_key
   region     = var.region
 }
